111190b514c7c04f76d742620bf5f76e5274214f
Paul156551 <pavel.ryzhkov.99@mail.ru>
2023-07-25 17:17:00 +0300

    Параметры для шара.

---
+++ lib/resources/images.dart
@@ -0,0 +1,12 @@
+part of 'resources.dart';
+
+class Images {
+  Images._();
+
+  static const String ellipse6 = 'docs/assets/Ellipse 6.png';
+  static const String ellipse7 = 'docs/assets/Ellipse 7.png';
+  static const String ball = 'docs/assets/ball.png';
+  static const String redBall = 'docs/assets/red ball.png';
+  static const String smallStar = 'docs/assets/small star.png';
+  static const String star = 'docs/assets/star.png';
+}
